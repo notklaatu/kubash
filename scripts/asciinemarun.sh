c7ee59e267d7e91dae833795ff40c7d723dad1b3
Josh Cox <josh@webhosting.coop>
2021-07-02 18:58:45 -0500

    Adding in a tmp dir

--- scripts/asciinemarun.sh
+++ scripts/asciinemarun.sh
@@ -2,6 +2,7 @@
 TARGET_OS=bionic
 TARGET_VERSION=1.21.2
 CLUSTER_NAME=kubash_test123
+TMP=$(mktemp -d --suffix=.dir /tmp/AsciinemaRun.XXXXX)
 
 runner () {
   COMMAND_2_RUN=$1
@@ -10,12 +11,13 @@ runner () {
   asciinema \
     rec \
     --quiet \
-    --title $TITLE_4_CAST \
-    --idle-time-limit 2.5 \
-    --command $COMMAND_2_RUN \
-    $FILE_2_SAVE
+    --title "$TITLE_4_CAST" \
+    --idle-time-limit "2.5" \
+    --command "$COMMAND_2_RUN" \
+    "$FILE_2_SAVE"
 }
 
-runner  "kubash build -y --target-os ${TARGET_OS}${TARGET_VERSION}" build.json "Kubash build run"
-runner  "kubash yaml2cluster -n $CLUSTER_NAME" yaml2cluster.json "Kubash yaml2cluster run"
-runner  "kubash provision -n $CLUSTER_NAME" provision.json "Kubash provision run"
+runner "kubash build -y --target-os ${TARGET_OS}${TARGET_VERSION}" $TMP/build.json "Kubash build run"
+runner "kubash yaml2cluster -n $CLUSTER_NAME" $TMP/yaml2cluster.json "Kubash yaml2cluster run"
+runner "kubash provision -n $CLUSTER_NAME" $TMP/provision.json "Kubash provision run"
+find $TMP -iname '*.json' 
